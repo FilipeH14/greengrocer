996dbec9e7c4e92e584e531ceb957385a7d4472f
Filipe Haniel Martins <filipe.martins@qwti.net.br>
2022-09-11 00:58:16 -0300

    badge e campo de pesquisa

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:badges/badges.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(fontSize: 30),
+            children: [
+              TextSpan(
+                text: 'Green',
+                style: TextStyle(color: CustomColors.customSwatchColor),
+              ),
+              TextSpan(
+                text: 'grocer',
+                style: TextStyle(color: CustomColors.customContrastColor),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: Badge(
+              badgeColor: CustomColors.customContrastColor,
+              badgeContent: const Text(
+                '2',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+              child: GestureDetector(
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+                onTap: () {},
+              ),
+            ),
+          ),
+        ],
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(width: 0, style: BorderStyle.none),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
