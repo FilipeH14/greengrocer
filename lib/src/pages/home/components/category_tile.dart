88564746d611e56679c011ea4a816921e4df8ec6
Filipe Haniel Martins <filipe.martins@qwti.net.br>
2022-09-18 11:34:52 -0300

    finalização tela home

---
+++ lib/src/pages/home/components/category_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class CategoryTile extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
+  const CategoryTile({
+    required this.onPressed,
+    required this.category,
+    required this.isSelected,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 6),
+          decoration: BoxDecoration(
+            color:
+                isSelected ? CustomColors.customSwatchColor : Colors.transparent,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            category,
+            style: TextStyle(
+                color:
+                    isSelected ? Colors.white : CustomColors.customContrastColor,
+                fontSize: isSelected ? 16 : 14,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:badges/badges.dart';
+import 'package:greengrocer/src/pages/home/components/category_tile.dart';
+import 'package:greengrocer/src/config/app_data.dart' as app_data;
+import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -77,8 +87,41 @@ class HomeTab extends StatelessWidget {
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(60),
-                  borderSide: const BorderSide(width: 0, style: BorderStyle.none),
+                  borderSide:
+                      const BorderSide(width: 0, style: BorderStyle.none),
                 ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: app_data.categories.length,
+              separatorBuilder: (_, index) => const SizedBox(width: 10),
+              itemBuilder: (_, index) => CategoryTile(
+                category: app_data.categories[index],
+                isSelected: app_data.categories[index] == selectedCategory,
+                onPressed: () => setState(() {
+                  selectedCategory = app_data.categories[index];
+                }),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 9 / 11.5,
+              ),
+              itemCount: app_data.items.length,
+              itemBuilder: (_, index) => ItemTile(
+                item: app_data.items[index],
               ),
             ),
           ),
