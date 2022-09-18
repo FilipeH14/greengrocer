4951ff3d648f5e6671cc778c0b4846e5d3837a54
Filipe Haniel Martins <filipe.martins@qwti.net.br>
2022-09-18 19:36:42 -0300

    tiles na tela de carrinho

---
+++ lib/src/pages/cart/cart_tab.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/pages/cart/components/cart_tile.dart';
+import 'package:greengrocer/src/services/utils_sevices.dart';
+import 'package:greengrocer/src/config/app_data.dart' as app_data;
+
+class CartTab extends StatelessWidget {
+  final UtilsServices utilsServices = UtilsServices();
+
+  CartTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Carrinho')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: app_data.cartItem.length,
+              itemBuilder: (_, index) => CartTile(cartItem: app_data.cartItem[index]),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 3,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Total geral',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  utilsServices.priceToCurrency(50.5),
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: CustomColors.customSwatchColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: CustomColors.customSwatchColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    child: const Text(
+                      'Concluir pedido',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/cart/components/cart_tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/models/cart_item_model.dart';
+import 'package:greengrocer/src/pages/common_widgets/quantity_widget.dart';
+import 'package:greengrocer/src/services/utils_sevices.dart';
+
+class CartTile extends StatelessWidget {
+  final CartItemModel cartItem;
+
+  final UtilsServices utilsServices = UtilsServices();
+
+  CartTile({
+    required this.cartItem,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListTile(
+        leading: Image.asset(
+          cartItem.item.imgUrl,
+          height: 60,
+          width: 60,
+        ),
+        title: Text(
+          cartItem.item.itemName,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: Text(
+          utilsServices.priceToCurrency(cartItem.totalPrice()),
+          style: TextStyle(
+            color: CustomColors.customSwatchColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: QuantityWidget(
+          value: cartItem.quantity,
+          suffixText: cartItem.item.unit,
+          result: (quantity) {},
+        ),
+      ),
+    );
+  }
+}
